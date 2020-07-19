f2e082c959945f23be5eb5bedb73344c47cbbad0
Moradisten <moradisten>
2020-07-19 13:40:27 +0200

    sdfa

--- launchers/launchCrossvalidation.sh
+++ launchers/launchCrossvalidation.sh
@@ -1,8 +1,3 @@
-CUR_DIR=$(cd .. && pwd)
-DATASET_DIR=$CUR_DIR/datasets/$1/
-python3 ../crossvalidation.py $CUR_DIR $1 $DATASET_DIR/$1_TRAIN.arff -window=2 -d=2.4 -v=4.2
-
-
 #!/bin/bash
 CUR_DIR=$(cd .. && pwd)
 for d in $CUR_DIR/datasets/*; do
@@ -17,7 +12,7 @@ for d in $CUR_DIR/datasets/*; do
         for v in 1 2 3 4 5 6 7 8 9 10
         do
           echo "Executing dataset [ $dataset ]  ..."
-          python3 ../crossvalidation.py $CUR_DIR -name=$dataset -train=$DATASET_DIR/"$dataset"_TRAIN.arff $window $v $neighbors
+          python3 ../crossvalidation.py $CUR_DIR $dataset $DATASET_DIR/"$dataset"_TRAIN.arff $window $v $neighbors
           echo "Classification process [ $dataset ] finished"
         done
       done
