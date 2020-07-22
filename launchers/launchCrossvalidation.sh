862fe1606b2222941fcd39a70b9c4deda841c6c2
Moradisten <moradisten>
2020-07-22 03:27:56 +0200

    Ejecutando crossvalidacion

--- launchers/launchCrossvalidation.sh
+++ launchers/launchCrossvalidation.sh
@@ -5,17 +5,8 @@ for d in $CUR_DIR/datasets/*; do
     dataset=$(basename $d)
     DATASET_DIR=$CUR_DIR/datasets/$dataset
     echo ""
-    for neighbors in 1 2 3 4 5 6 7 8 9 10
-    do
-      for window in 1 2 3 4 5 6 7 8 9 10
-      do
-        for v in 1 2 3 4 5 6 7 8 9 10
-        do
-          echo "Executing dataset [ $dataset ]  ..."
-          python3 ../crossvalidation.py $CUR_DIR $dataset $DATASET_DIR/"$dataset"_TRAIN.arff $window $v $neighbors
-          echo "Classification process [ $dataset ] finished"
-        done
-      done
-    done
+    echo "Executing dataset [ $dataset ]  ..."
+    python3 ../crossvalidation.py $CUR_DIR $dataset $DATASET_DIR/"$dataset"_TRAIN.arff 1 20 1
+    echo "Classification process [ $dataset ] finished"
   fi
 done
